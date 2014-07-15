f7fa962d7f7afbc11de3b76d2a2678775d634f6f
Odilon Alves de Oliveira <odilontalk@gmail.com>
2014-07-14 21:59:35 -0300

    Adding CSRF Token for Ajax Requests

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
     $(".removeLink").on("click", function() {
         $.ajax({
             type: "DELETE",
-            url: $(this).data("remove-uri") + "/" + $(this).data("remove-id")
+            url: $(this).data("remove-uri") + "/" + $(this).data("remove-id"),
+            beforeSend: function(xhr) {
+                var header = $("meta[name='_csrf_header']").attr("content");
+                var token = $("meta[name='_csrf']").attr("content");
+                xhr.setRequestHeader(header, token);
+            }
         }).done(function() {
             alert("Registro apagado com sucesso !");
             location.reload();
