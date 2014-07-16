00c3e5cf666efa7d9a1466439266c3ccee28038b
Odilon Alves de Oliveira <odilontalk@gmail.com>
2014-07-15 23:50:13 -0300

    Fixing Integration Tests

--- src/test/java/config/FissionTestConfig.java
+++ src/test/java/config/FissionTestConfig.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.support.PersistenceExceptionTranslator;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.JpaDialect;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -23,9 +26,9 @@ public class FissionTestConfig {
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setDataSource(this.datasource());
-        entityManagerFactory.setPersistenceUnitName("fissionPU-Test");
-
+        entityManagerFactory.setPersistenceUnitName("fissionPU-test");
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setJpaDialect(new HibernateJpaDialect());
         return entityManagerFactory;
     }
 
@@ -34,7 +37,6 @@ public class FissionTestConfig {
         return new HibernateExceptionTranslator();
     }
 
-
     @Bean
     public PlatformTransactionManager transactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
@@ -42,9 +44,8 @@ public class FissionTestConfig {
         return transactionManager;
     }
 
-
     @Bean
-    public DataSource datasource() {
+    public DataSource dataSource() {
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setDriverClassName("org.h2.Driver");
         basicDataSource.setUrl("jdbc:h2:mem:fission");
